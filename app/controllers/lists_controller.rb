5375cc2be7f4e6e303eeec0d70471c6c5149fe32
Anyssa Jerbi <anyssa.jerbi@gmail.com>
2023-05-26 13:45:52 +0200

    movies site

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,11 +1,12 @@
 class ListsController < ApplicationController
-  before_action :set_list, only: [:show, :edit, :update, :destroy]
+  before_action :set_list, only: [:show, :destroy]
 
   def index
     @lists = List.all
   end
 
   def show
+    @bookmark = Bookmark.new
   end
 
   def new
@@ -15,26 +16,15 @@ class ListsController < ApplicationController
   def create
     @list = List.new(list_params)
     if @list.save
-      redirect_to @list, notice: 'List was successfully created.'
+      redirect_to list_path(@list)
     else
-      render :new
-    end
-  end
-
-  def edit
-  end
-
-  def update
-    if @list.update(list_params)
-      redirect_to @list, notice: 'List was successfully updated.'
-    else
-      render :edit
+      render :new, status: :unprocessable_entity
     end
   end
 
   def destroy
     @list.destroy
-    redirect_to lists_url, notice: 'List was successfully destroyed.'
+    redirect_to lists_path, status: :see_other
   end
 
   private
@@ -44,56 +34,6 @@ class ListsController < ApplicationController
   end
 
   def list_params
-    params.require(:list).permit(:name)
+    params.require(:list).permit(:name, :photo)
   end
 end
-ruby
-Copy code
-# app/controllers/bookmarks_controller.rb
-class BookmarksController < ApplicationController
-  before_action :set_bookmark, only: [:show, :edit, :update, :destroy]
-
-  def index
-    @bookmarks = Bookmark.all
-  end
-
-  def show
-  end
-
-  def new
-    @bookmark = Bookmark.new
-  end
-
-  def create
-    @bookmark = Bookmark.new(bookmark_params)
-    if @bookmark.save
-      redirect_to @bookmark, notice: 'Bookmark was successfully created.'
-    else
-      render :new
-    end
-  end
-
-  def edit
-  end
-
-  def update
-    if @bookmark.update(bookmark_params)
-      redirect_to @bookmark, notice: 'Bookmark was successfully updated.'
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @bookmark.destroy
-    redirect_to bookmarks_url, notice: 'Bookmark was successfully destroyed.'
-  end
-
-  private
-
-  def set_bookmark
-    @bookmark = Bookmark.find(params[:id])
-  end
-
-  def bookmark_params
-    params.require(:bookmark).permit(:comment, :movie_id, :list_id
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :movies
-  resources :lists
-  resources :bookmarks, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "lists#index"
+  resources :lists, except: [:edit, :update] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: :destroy
 end
